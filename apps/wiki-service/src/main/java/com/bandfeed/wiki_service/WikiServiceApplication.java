7163446286b2d1791f4a017817d17ac6e4a1c477
jaeHyun <dacapo629@naver.com>
2026-06-07 19:11:06 +0900

    chore:user, wiki, chat 서비스 초기 설정 추가

---
+++ apps/wiki-service/src/main/java/com/bandfeed/wiki_service/WikiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bandfeed.wiki_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiServiceApplication.class, args);
+	}
+
+}
