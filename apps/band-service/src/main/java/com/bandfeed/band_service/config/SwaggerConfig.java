95107b60f56fe0e40371da4ac3c453772f53cfb4
RunSBS <dacapo629@naver.com>
2026-06-09 18:23:57 +0900

    feat: Swagger config 추가

---
+++ apps/band-service/src/main/java/com/bandfeed/band_service/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.bandfeed.band_service.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Band Service API")
+                        .description("밴드 생성·관리·타임라인 API")
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Auth"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Auth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
