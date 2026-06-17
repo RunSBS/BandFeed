d35107196062faa521a1c4ca150828f9a4eb9c3f
jaeHyun <dacapo629@naver.com>
2026-06-17 20:42:14 +0900

    feat: 게이트웨이 cors 설정 추가

--- apps/gateway-service/src/main/java/com/bandfeed/gateway_server/security/SecurityConfig.java
+++ apps/gateway-service/src/main/java/com/bandfeed/gateway_server/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://3.35.71.18"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
